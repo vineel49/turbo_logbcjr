5c12791ffde500674b17b40030a42f05aa7f72a1
Vineel Kumar Veludandi <vkveludandi@gmail.com>
2021-02-05 14:58:37 +0530

    Add files via upload

---
+++ Get_Trellis.m
@@ -0,0 +1,10 @@
+% Trellis for the Convolutional encoder with generator matrix given by
+% [1 (1+D^2)/(1+D+D^2)]
+function [Prev_State,Prev_Ip,Outputs_prev,Next_State,Outputs_next,Next_Ip]= Get_Trellis()
+Outputs_prev = [1,4; 2,3; 1,4; 2,3];  % Gamma indices for alpha recursion
+Prev_State = [1,2; 4,3; 2,1; 3,4]; % previous state
+Prev_Ip =  [1,2;1,2;1,2;1,2] ; % Previous input.
+Next_State = [1,3; 3,1; 4,2; 2,4]; % Next state
+Outputs_next = [1,4; 1,4; 2,3; 2,3]; % Gamma indices for beta recursion
+Next_Ip = [1,2;1,2;1,2;1,2];% Next input
+end
